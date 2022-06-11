eeabdf3f3f0a7bf1dd48f96a25d17cc1d51b9b52
PkuCuipy <1900017785@pku.edu.cn>
2022-06-12 00:11:59 +0800

    Imported by DOM-paste done. (TODO: imported by fetching data from PKUHelper-API)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { Component } from "react";
 import import_icon from "./icons/import.svg";
 import warning_icon from "./icons/warning.svg";
 import add_icon from "./icons/add.svg";
+import {
+  seemsByToken, seemsByPageCopy,
+  fetchCourseInfoAll, parseCourseInfoAll,
+  calcAvgGPA,
+} from "./util.js";
 
 function TitleBar() {
   return (
@@ -21,10 +26,9 @@ function BottomBar() {
   );
 }
 
-
 function Button(props) {
   return (
-    <span className="settings-button">
+    <span className="settings-button" onClick={props.onClick}>
       <img src={props.icon} alt="import-icon" width="16" height="16" style={{marginRight: ".2rem"}}></img>
       <span> {props.name} </span>
     </span>
@@ -32,29 +36,38 @@ function Button(props) {
 }
 
 
-function Settings() {
+function Settings(props) {
   return (
     <div id={"settings"}>
-      <Button name={"重新导入"} icon={import_icon}/>
-      <Button name={"编辑警告"} icon={warning_icon}/>
-      <Button name={"新建课程"} icon={add_icon}/>
+      <Button name={"重新导入"} icon={import_icon} onClick={props.onClickImport}/>
+      <Button name={"隐藏/显示编辑警告"} icon={warning_icon} onClick={props.onToggleEditedWarning}/>
+      <Button name={"添加课程"} icon={add_icon} onClick={props.onNewCourse}/>
     </div>
   )
 }
 
 
-class Importer extends Component {
-  render() {
-    return (
-      <div> This is Importer  </div>
-    );
-  }
+function Importer(props) {
+  return (
+    <div id={"importer"}>
+      <div id={"import-prompt"}>
+        😀 请在下方输入框内粘贴以导入您的课程成绩信息:
+        <ul>
+          <li><div><strong>方式1:</strong> 复制您的 PKU Helper token, 粘贴到下方;</div></li>
+          <li><div><strong>方式2:</strong> 全选 PKU Helper 成绩查询页面并复制, 粘贴到下方;</div></li>
+        </ul>
+      </div>
+      <div id={"paste-here"} contentEditable={"true"} onInput={props.onPaste}>
+      </div>
+    </div>
+  );
 }
 
 
 class GradeBook extends Component {
   render() {
     return (
+      /* TODO */
       <div> This is Grade Book </div>
     );
   }
@@ -64,11 +77,11 @@ class GradeBook extends Component {
 class Summary extends Component {
   render() {
     return (
+      /* TODO */
       <div> This is Summary  </div>
     );
   }
 }
-
 
 
 
@@ -82,13 +95,64 @@ class App extends Component{
     }
   }
 
+
+  /* 清空 ｢粘贴区域｣ 中的内容 */
+  clearPasteArea = () => {
+    let paste_area = document.getElementById("paste-here");
+    paste_area.innerHTML = "";
+  }
+
+  /* 当用户点击 ｢重新导入｣ 按钮时的行为 */
+  handleImport = () => {
+    console.log("请求重新导入数据");
+    this.setState({ need_initial_import: true });
+  }
+
+  /* 当用户点击 ｢编辑警告｣ 按钮时的行为 */
+  handleToggleEditedWarning = () => {
+    console.log(`从 ${this.state.ignore_edited_warning} 切换为 ${!this.state.ignore_edited_warning}`);
+    this.setState({ ignore_edited_warning: !this.state.ignore_edited_warning });
+  }
+
+  /* 当用户点击 ｢添加课程｣ 按钮时的行为 */
+  handleNewCourse = () => {
+    /* TODO */
+    console.log("请求添加一门新的课程");
+  }
+
+  /* 当用户向 ｢粘贴区域｣ 粘贴时的行为 */
+  handlePaste = (evt) => {
+    console.log("用户粘贴了一些东西, 来看看能不能解析?");
+    let elem = evt.target;    // 这个理应就是 #paste-here
+    // 判断 ｢导入方式｣
+    if (seemsByToken(elem)) {   // 粘贴的是 ｢token｣ 类似物
+      /* TODO */
+      fetchCourseInfoAll(elem.innerText);
+    }
+    else if (seemsByPageCopy(elem)) {   // 粘贴的是 ｢成绩查询页面｣ 类似物
+      let infos = parseCourseInfoAll(elem);
+      console.log("infos:", infos);
+      console.log("gpa:", calcAvgGPA(infos));
+    }
+    else {
+      console.log("无法识别您粘贴的内容w 请仔细阅读说明后重试🥺")
+    }
+
+    this.clearPasteArea();
+    console.log("用户粘贴的内容已清空~");
+  }
+
   render() {
     return (
       <>
         <TitleBar/>
-        <Settings/>
+        <Settings
+          onClickImport={this.handleImport}
+          onToggleEditedWarning={this.handleToggleEditedWarning}
+          onNewCourse={this.handleNewCourse}
+        />
         {this.state.need_initial_import
-          ? <Importer/>
+          ? <Importer onPaste={this.handlePaste}/>
           : <><GradeBook/><Summary/></>}
         <BottomBar/>
       </>
--- src/util.js
+++ src/util.js
@@ -1,3 +1,17 @@
+function seemsByToken(elem) {
+  return elem.innerText.match(/[a-z0-9]{32}/);
+}
+
+function seemsByPageCopy(elem) {
+  return (elem.getElementsByClassName("semester-block").length !== 0);
+}
+
+
+function fetchCourseInfoAll(token, callback) {
+  //TODO: 用 token 构造 url 去请求数据!
+}
+
+
 function getDOMChild(node, indices) {
   // indices 如 [1,0,0,0,1,2]
   let ret = node;
@@ -7,28 +21,34 @@ function getDOMChild(node, indices) {
   return ret;
 }
 
-function calcGPA(grade) {
-  return 4 - 3 * (100 - grade) ** 2 / 1600;
-}
-
-function getCourseInfoAll(DOMElem) {
+function parseCourseInfoAll(DOMElem) {
   // 统计所有课程信息
   let course_infos = [];
   let semester_blocks = [...DOMElem.getElementsByClassName("semester-block")];
-  console.log(DOMElem.getElementsByClassName("semester-block"));
+  // console.log(DOMElem.getElementsByClassName("semester-block"));
   for (let block of semester_blocks.slice(0, -1)) {   // 最后一项是 "总绩点", 不是一个 "学期", 因此丢掉
                                                       // console.log(block);
     let semester_name = getDOMChild(block, [0,0,1,0,0,0]).innerText;
     let course_rows = block.getElementsByClassName("course-row");
     for (let row of course_rows) {
       // console.log(row);
+      let name = getDOMChild(row, [0,1,0,0,0,0]).innerText;
       let semester = semester_name;
       let credit = Number(getDOMChild(row, [0,0,0,0,0]).innerText);
-      let grade = row.getElementsByClassName("score-tamperer")[0].value;
+      let score = row.getElementsByClassName("score-tamperer")[0].value;
       let type_teacher = getDOMChild(row, [0,1,0,0,1]).innerText.split("-");
       let type = type_teacher[0].trim();
       let teacher = type_teacher[1].trim();
-      let course_info = {semester, credit, grade, type, teacher};
+      let course_info = {
+        name,
+        semester,
+        credit,
+        score,
+        type,
+        teacher,
+        is_user_created: false,
+        edited_score: score,
+      };
       // console.log(course_info);
       course_infos.push(course_info);
     }
@@ -43,42 +63,44 @@ function calcAvgGPA(course_infos) {
   let creditTotal = 0.0;
   let GPATotal = 0.0;
   for (let info of course_infos) {
-    let grade = parseFloat(info.grade)
-    if (isNaN(grade)) {
-      if (info.grade === "P") { }
-      else if (info.grade === "F") { }
-      else if (info.grade === "W") { }
+    let score = parseFloat(info.score)
+    if (isNaN(score)) {
+      if (info.score === "P") { }
+      else if (info.score === "F") { }
+      else if (info.score === "W") { }
       else { throw "无法解析成绩"; }
     }
     else {
-      if (grade >= 60 && grade <= 100) {
+      if (score >= 60 && score <= 100) {
         let credit = info.credit;
-        let GPA = calcGPA(grade);
+        let GPA = calcGPA(score);
         GPATotal += GPA * credit;
         creditTotal += credit;
       }
     }
   }
-
-  let avgGPA = GPATotal / creditTotal;  // 加权平均
-  return avgGPA;
+  return GPATotal / creditTotal;  // 加权平均
 }
 
-function handlePaste(e) {
-  let elem = e.target;
-  let infos = getCourseInfoAll(elem);
-  calcAvgGPA(infos)
 
-  let container = document.getElementById("container");
-  container.removeChild(elem);
-  let new_pastebin = document.createElement("div");
-  new_pastebin.id = "paste-here";
-  new_pastebin.contentEditable = "true";
-  new_pastebin.oninput = handlePaste;
-  container.appendChild(new_pastebin);
+
+function calcGPA(grade) {
+  return 4 - 3 * (100 - grade) ** 2 / 1600;
+}
+
+
+
+
+export {
+  fetchCourseInfoAll,
+  seemsByToken,
+  seemsByPageCopy,
+  parseCourseInfoAll,
+  calcGPA,
+  calcAvgGPA,
 }
 
 /* =============================================================== */
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("paste-here").oninput = handlePaste;
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   document.getElementById("paste-here").oninput = handlePaste;
+// });
