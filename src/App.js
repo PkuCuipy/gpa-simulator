fde87d9fe8eab256d39ffc17b0e8e4020661a339
PkuCuipy <1900017785@pku.edu.cn>
2022-06-11 21:49:36 +0800

    BottomBar Done!

--- src/App.js
+++ src/App.js
@@ -8,12 +8,27 @@ function TitleBar() {
   );
 }
 
+function BottomBar() {
+  return (
+    <div id={"bottom-bar"}>
+      <div> 绩点公式: GPA(x) = 4 - 3(100 - x)² / 1600 </div>
+      <div> ️⚠︎ 本页面结果仅供参考, 请以学校官方结果为准! </div>
+    </div>
+  );
+}
+
+
+
 
 
 
 function App() {
   return (
-    <TitleBar/>
+    <>
+      <TitleBar/>
+      <BottomBar/>
+
+    </>
   )
 }
 
