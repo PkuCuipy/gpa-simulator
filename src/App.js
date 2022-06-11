0594c218240e065b477500d8aee385d03bbbb0c7
PkuCuipy <1900017785@pku.edu.cn>
2022-06-11 22:33:20 +0800

    Skeleton Complete.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import "./App.css";
+import { Component } from "react";
+import import_icon from "./icons/import.svg";
+import warning_icon from "./icons/warning.svg";
+import add_icon from "./icons/add.svg";
 
 function TitleBar() {
   return (
@@ -18,18 +22,78 @@ function BottomBar() {
 }
 
 
-
-
-
-
-function App() {
+function Button(props) {
   return (
-    <>
-      <TitleBar/>
-      <BottomBar/>
-
-    </>
+    <span className="settings-button">
+      <img src={props.icon} alt="import-icon" width="16" height="16" style={{marginRight: ".2rem"}}></img>
+      <span> {props.name} </span>
+    </span>
   )
+}
+
+
+function Settings() {
+  return (
+    <div id={"settings"}>
+      <Button name={"重新导入"} icon={import_icon}/>
+      <Button name={"编辑警告"} icon={warning_icon}/>
+      <Button name={"新建课程"} icon={add_icon}/>
+    </div>
+  )
+}
+
+
+class Importer extends Component {
+  render() {
+    return (
+      <div> This is Importer  </div>
+    );
+  }
+}
+
+
+class GradeBook extends Component {
+  render() {
+    return (
+      <div> This is Grade Book </div>
+    );
+  }
+}
+
+
+class Summary extends Component {
+  render() {
+    return (
+      <div> This is Summary  </div>
+    );
+  }
+}
+
+
+
+
+class App extends Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      need_initial_import: true,
+      ignore_edited_warning: false,
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <TitleBar/>
+        <Settings/>
+        {this.state.need_initial_import
+          ? <Importer/>
+          : <><GradeBook/><Summary/></>}
+        <BottomBar/>
+      </>
+    );
+  }
 }
 
 
